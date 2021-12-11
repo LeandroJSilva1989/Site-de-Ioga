5984611c818387dc8e02e629b49d09678fe486e4
LeandroJSilva1989 <lelejs@hotmaill.com>
2021-12-10 23:02:19 -0300

    Update main-toggle.js

--- CSS/components/main-toggle.js
+++ CSS/components/main-toggle.js
@@ -1,7 +1,6 @@
-const canvas = document.getElementById('#canvas');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.beginStyle = 'red';
 ctx.beginPath();
 ctx.moveTo(30, 30);
 ctx.bezierCurveTo(120,160, 180,10, 220,140);
