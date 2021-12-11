08cd4a4b475eceaf3103924999ee037db646978d
LeandroJSilva1989 <lelejs@hotmaill.com>
2021-12-10 23:25:07 -0300

    Update main-toggle.js

--- CSS/components/main-toggle.js
+++ CSS/components/main-toggle.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('#canvas');
 const ctx = canvas.getContext('2d');
 
-let corLinha = "red";
-canvas = corLinha;
+ctx.lineWidth = 2;
+ctx.strokeStyle = "#FF1448";
 ctx.beginPath();
 ctx.moveTo(30, 30);
 ctx.bezierCurveTo(120,160, 180,10, 220,140);
