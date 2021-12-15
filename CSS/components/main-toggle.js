7deb5e119353c5197a84f83ff329244b02034b51
LeandroJSilva1989 <lelejs@hotmaill.com>
2021-12-15 20:53:25 -0300

    Update main-toggle.js

--- CSS/components/main-toggle.js
+++ CSS/components/main-toggle.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('#canvas');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 ctx.lineWidth = 2;
@@ -7,4 +7,4 @@ ctx.setLineDash([5, 15]);
 ctx.beginPath();
 ctx.moveTo(30, 30);
 ctx.bezierCurveTo(120,160, 180,10, 220,140);
-ctx.strokeRect();
+ctx.stroke();
