3a86037c3de7b9f3d79969fc9f52750403d81067
LeandroJSilva1989 <leandrojsilva1989@hotmail.com>
2023-01-24 20:34:42 -0300

    Refatorando o site 24

--- Script/components/change-theme.js
+++ Script/components/change-theme.js
@@ -1,3 +1,16 @@
-function changeTheme() {
-  document.body.classList.toggle("dark");
+$(document).ready(function() {
+  $("#color_mode").on("change", function () {
+      colorModePreview(this);
+  })
+});
+
+function colorModePreview(ele) {
+  if($(ele).prop("checked") == true){
+      $('body').addClass('dark-preview');
+      $('body').removeClass('white-preview');
+  }
+  else if($(ele).prop("checked") == false){
+      $('body').addClass('white-preview');
+      $('body').removeClass('dark-preview');
+  }
 }
