efe3692c2efb24e8c26671029e1a15d4d113b7af
LeandroJSilva1989 <leandrojsilva1989@hotmail.com>
2023-01-25 23:13:54 -0300

    Refatorando o site 41

--- Script/components/change-theme.js
+++ Script/components/change-theme.js
@@ -7,10 +7,10 @@ $(document).ready(function() {
 function colorModePreview(ele) {
   if($(ele).prop("checked") == true){
       $('body').addClass('dark-preview');
-      $('body').removeClass('white-preview');
+      
   }
   else if($(ele).prop("checked") == false){
-      $('body').addClass('white-preview');
+      
       $('body').removeClass('dark-preview');
   }
 }
