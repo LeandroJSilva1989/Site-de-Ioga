27a7d1efb14919322138597c0bb26ff479d7ee5b
LeandroJSilva1989 <leandrojsilva1989@hotmail.com>
2023-01-28 22:38:47 -0300

    refatorando o dark-mode 456854

--- Script/components/change-theme.js
+++ Script/components/change-theme.js
@@ -32,8 +32,21 @@
          document.body.classList.toggle("dark");
        }*/
 
+         
+
 
        function changeTheme() {
          var element = document.getElementById("darkBg");
          element.classList.toggle("dark");
+
+         var myCheck = document.getElementById("myCheck");
+         var img = document.querySelector("#menu-dark");
+
+         if ( myCheck.checked === true) {
+            img.setAttribute('src', 'https://leandrojsilva1989.github.io/Site-de-Ioga/assets/img/list-dark.png');
+         } else {
+            img.setAttribute('src', 'https://leandrojsilva1989.github.io/Site-de-Ioga/assets/img/list.png');
+         }
+
+
       }
