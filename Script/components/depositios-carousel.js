b657a73d4401456e306ce2c0c5101779c7344eea
LeandroJSilva1989 <85590476+LeandroJSilva1989@users.noreply.github.com>
2023-02-08 15:05:04 -0300

    adiciona o bloqueio dos botões do carrossel

--- Script/components/depositios-carousel.js
+++ Script/components/depositios-carousel.js
@@ -3,6 +3,8 @@ const btnRight = window.document.querySelector(".btn-arrow-right");
  const elements = window.document.querySelector(".elements");
  let pixels = 597;
 
+ 
+
  btnRight.addEventListener("click", function() {
    pixels = pixels - 597;
    elements.style = `transform: translateX(${pixels}px)`;
@@ -30,19 +32,15 @@ buttonsWrapper.addEventListener("click", e => {
     if (e.target.classList.contains("first")) {
       slides.style.transform = "translateX(-0%)";
       e.target.classList.add("active");
+      document.getElementById("previewDepositions").disable = true;
     } else if (e.target.classList.contains("second")) {
-      slides.style.transform = "translateX(-22.4%)";
+      slides.style.transform = "translateX(-22.3%)";
       e.target.classList.add("active");
     } else if (e.target.classList.contains('third')){
       slides.style.transform = 'translatex(-39.8%)';
       e.target.classList.add('active');
-    } else if (e.target.classList.contains('fourth')){
-      slides.style.transform = 'translatex(-59.7%)';
-      e.target.classList.add('active');
-    } else if (e.target.classList.contains('fifth')){
-      slides.style.transform = 'translatex(-79.6%)';
-      e.target.classList.add('active');
-    }
+      document.getElementById("nextDepositions").disable = true;
+    } 
   }
 });
 
