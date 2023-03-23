143047c497350c3f717551bde2070f970bf6cb26
LeandroJSilva1989 <85590476+LeandroJSilva1989@users.noreply.github.com>
2023-03-22 23:26:04 -0300

    Refatorando o responsivel 2326

--- Script/components/side-panel.js
+++ Script/components/side-panel.js
@@ -1,7 +1,6 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
     document.getElementById("mySidePanel").style.width = "250px";
-    document.getElementsByClassName("close-btn").display = "block"
   }
   
   
