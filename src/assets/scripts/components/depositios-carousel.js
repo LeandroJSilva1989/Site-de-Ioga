1ce7862bd3c3a3b0f08dba1053a775500f5e765d
LeandroJSilva1989 <leandrojsilva1989@hotmail.com>
2023-04-15 19:19:53 -0300

    Refatorando o site

--- src/assets/scripts/components/depositios-carousel.js
+++ src/assets/scripts/components/depositios-carousel.js
@@ -1,21 +1,21 @@
 
 const btnRight = document.querySelector(".btn-arrow-right");
- const btnLeft = document.querySelector(".btn-arrow-left");
- const elements = document.querySelector(".elements");
- let pixels = 0;
+const btnLeft = document.querySelector(".btn-arrow-left");
+const elements = document.querySelector(".elements");
+let pixels = 0;
 
 
- 
 
- btnRight.addEventListener("click", function() {
-   pixels = pixels - 22.3;
-   elements.style = `transform: translateX(${pixels}%)`;
-   
- });
 
- btnLeft.addEventListener("click", function() {
-   pixels = pixels + 22.3;
-   elements.style = `transform: translateX(${pixels}%)`;
+btnRight.addEventListener("click", function () {
+  pixels = pixels - 22.3;
+  elements.style = `transform: translateX(${pixels}%)`;
+
+});
+
+btnLeft.addEventListener("click", function () {
+  pixels = pixels + 22.3;
+  elements.style = `transform: translateX(${pixels}%)`;
 });
 
 
@@ -25,24 +25,24 @@ const slides = document.querySelector(".depositions-carousel");
 
 
 buttonsWrapper.addEventListener("click", e => {
-  
-  
+
+
   if (e.target.nodeName === "BUTTON") {
     Array.from(buttonsWrapper.children).forEach(item =>
       item.classList.remove("active")
     );
-    
+
     if (e.target.classList.contains("first")) {
       slides.style.transform = "translateX(-0%)";
       e.target.classList.add("active");
     } else if (e.target.classList.contains("second")) {
       slides.style.transform = "translateX(-22.3%)";
       e.target.classList.add("active");
-    } else if (e.target.classList.contains('third')){
+    } else if (e.target.classList.contains('third')) {
       slides.style.transform = 'translatex(-44.6%)';
       e.target.classList.add('active');
-      
-    } 
+
+    }
 
   }
 });
@@ -63,22 +63,25 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("depositions-carousel-slide");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "";  
+    slides[i].style.display = "";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace("active", "");
   }
-  slides[slideIndex-1].style.display = "flex";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
 }
 
 // recupera os elementos HTML relevantes
 const carousel = document.querySelector('.wrapper-depositions .depositions-carousel');
 const prevButton = document.getElementById('previewDepositions');
 const nextButton = document.getElementById('nextDepositions');
+const dotFirst = document.getElementById('first');
+const dotSecond = document.getElementById('second');
+
 
 
 // define o índice inicial do carrossel
@@ -104,18 +107,21 @@ function updateButtonStates() {
   if (currentSlideIndex <= 0) {
     // o carrossel está no início, desativa o botão "preview"
     prevButton.disabled = true;
+    dotFirst.disabled = true;
   } else {
     // o carrossel não está no início, ativa o botão "preview"
     prevButton.disabled = false;
-    
+    dotFirst.disabled = false;
   }
 
   if (currentSlideIndex >= carousel.children.length - 1) {
     // o carrossel está no final, desativa o botão "next"
     nextButton.disabled = true;
+    dotSecond.disabled = true;
   } else {
     // o carrossel não está no final, ativa o botão "next"
     nextButton.disabled = false;
+    dotSecond.disabled = false;
   }
 }
 
