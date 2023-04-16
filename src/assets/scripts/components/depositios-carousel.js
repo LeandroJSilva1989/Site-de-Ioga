391b28fbee05ca25e9058d7c072ba00bccc0eeab
LeandroJSilva1989 <leandrojsilva1989@hotmail.com>
2023-04-16 13:53:17 -0300

    Refatorando o site

--- src/assets/scripts/components/depositios-carousel.js
+++ src/assets/scripts/components/depositios-carousel.js
@@ -69,7 +69,7 @@ function showSlides(n) {
     slides[i].style.display = "";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
