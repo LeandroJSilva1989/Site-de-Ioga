ecaed7ed466484382f3b97da90a9b3f9e988e877
LeandroJSilva1989 <leandrojsilva1989@hotmail.com>
2023-04-29 19:25:51 -0300

    Refatorando o site

--- src/assets/scripts/components/depositios-carousel.js
+++ src/assets/scripts/components/depositios-carousel.js
@@ -99,7 +99,7 @@ function showSlides(n) {
   }
 
   // desativa o botão next se estiver no último slide
-  if (n == slides.length) {
+  if (n == slides.length -1) {
     nextButton.disabled = true;
   } else {
     nextButton.disabled = false;
